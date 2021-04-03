464ff514fc6470ac90e5ff13119d4f361052d3fe
Андрей Васильченко <info@obmen.market>
2021-04-03 22:22:04 +0300

    Rules

--- src/ObmenMarket/LotFull/LotFull.js
+++ src/ObmenMarket/LotFull/LotFull.js
@@ -1,5 +1,9 @@
 import { useState, useEffect, useRef } from "react";
+<<<<<<< Updated upstream
 import { db_offers, fb, fa, db } from "../../Utils/firebase";
+=======
+import { db_offers, fb, fa } from "../../Utils/firebase";
+>>>>>>> Stashed changes
 import { compose } from "redux";
 import { connect } from "react-redux";
 import { withRouter, Link } from "react-router-dom";
@@ -414,6 +418,7 @@ const LotFull = ({
   onOfferCancel,
   createOffer,
 }) => {
+  console.log(fa.currentUser);
   const [isOfferForm, setIsOfferForm] = useState(false);
 
   const handleOfferForm = () => {
