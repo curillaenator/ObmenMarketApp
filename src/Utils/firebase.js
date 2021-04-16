ce608c871222f632b47258040d010bc030e943cf
Андрей Васильченко <info@obmen.market>
2021-04-16 20:20:23 +0300

    FCM update

--- src/Utils/firebase.js
+++ src/Utils/firebase.js
@@ -45,19 +45,7 @@ export const an = firebase.analytics();
 
 export const messaging = app.messaging();
 
-messaging.getToken({ vapidKey: 'BOsXtfpHw1gYRFvpZ_bcpZvyRKlFtEJRdAmlcmK_aMdWq9YEsB30L2WKmpnGpe77jd0Cv5DFhjQKH9xHZoq2_fs' }).then((currentToken) => {
-  if (currentToken) {
-    // Send the token to your server and update the UI if necessary
-    // ...
-  } else {
-    // Show permission request UI
-    console.log('No registration token available. Request permission to generate one.');
-    // ...
-  }
-}).catch((err) => {
-  console.log('An error occurred while retrieving token. ', err);
-  // ...
-});
+
 
 // using SendGrid's Node.js Library - https://github.com/sendgrid/sendgrid-nodejs
 // export const sg = require("@sendgrid/mail")
